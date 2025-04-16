936f8df542a84f1e34a1a2c7f4c4f23f4c8135f6
Rodrigo Frutuoso <rodrigoafonsofrutuoso@gmail.com>
2025-04-16 15:32:29 +0100

    ajustes

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -18,7 +18,7 @@ function loadHeader() {
 }
 
 function loadFooter() {
-    fetch('components/footer.html') // Caminho atualizado
+    fetch('components/footer.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Error loading footer: ${response.status}`);
